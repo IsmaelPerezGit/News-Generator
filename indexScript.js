b7597ecc68155bd782094dd1340441151932346b
ismael perez <ismaelperezemail@gmail.com>
2017-07-18 15:57:06 -0700

    added button funcionality and story title in jumbotron

--- indexScript.js
+++ indexScript.js
@@ -1,24 +1,30 @@
-window.onload = function(){
+window.onload = function() {
   $.ajax({
-  url: "https://content.guardianapis.com/search?q=technology&api-key=3cacc75f-61ed-4cf7-b623-78c2af06d49d",
-  success: function(data){
+    url: "https://content.guardianapis.com/search?q=technology&api-key=3cacc75f-61ed-4cf7-b623-78c2af06d49d",
+    success: function(data) {
 
-    var articleArr = [];
-    var storyTitleArray = [];
+      var articleArr = [];
+      var storyTitleArray = [];
 
-    function randomTitleGetter() {
-      for (var i = 0; i < data.response.results.length; i++) {
-        var retrievedData = data.response.results;
-        var retrievedDataTitle = data.response.results[0].webTitle;
-        articleArr.push(retrievedData[i]);
-        storyTitleArray.push(articleArr[i].webTitle);
+      function randomTitleGetter() {
+        for (var i = 0; i < data.response.results.length; i++) {
+          var retrievedData = data.response.results;
+          var retrievedDataTitle = data.response.results[0].webTitle;
+
+          articleArr.push(retrievedData[i]);
+          storyTitleArray.push(articleArr[i].webTitle);
         }
         var randomTitle = storyTitleArray[Math.floor(Math.random() * storyTitleArray.length)];
         return randomTitle;
-        console.log(randomTitle);
-    }
-        console.log(randomTitleGetter());
-        document.getElementsByClassName("jumbotronText")[0].innerHTML = randomTitleGetter();
       }
+        console.log(randomTitleGetter());
+
+        document.getElementsByClassName("generateButton")[0].addEventListener("click", function() {
+        document.getElementsByClassName("jumbotronText")[0].innerHTML = randomTitleGetter();
+        document.getElementsByClassName("generateButton")[0].innerHTML = "Next Story";
+      });
+
+
+    }
   });
 };
