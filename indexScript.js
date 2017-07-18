367385d4fdc5627e3b04d8df2a1e852cae32a671
ismael perez <ismaelperezemail@gmail.com>
2017-07-18 13:32:56 -0700

    added javascript functionality

--- indexScript.js
+++ indexScript.js
@@ -1,8 +1,24 @@
-document.addEventListener('load', function(){
+window.onload = function(){
   $.ajax({
-  url: "http://content.guardianapis.com/search?section=technology&api-key=3cacc75f-61ed-4cf7-b623-78c2af06d49d",
+  url: "https://content.guardianapis.com/search?q=technology&api-key=3cacc75f-61ed-4cf7-b623-78c2af06d49d",
   success: function(data){
-    console.log(data);
+
+    var articleArr = [];
+    var storyTitleArray = [];
+
+    function randomTitleGetter() {
+      for (var i = 0; i < data.response.results.length; i++) {
+        var retrievedData = data.response.results;
+        var retrievedDataTitle = data.response.results[0].webTitle;
+        articleArr.push(retrievedData[i]);
+        storyTitleArray.push(articleArr[i].webTitle);
+        }
+        var randomTitle = storyTitleArray[Math.floor(Math.random() * storyTitleArray.length)];
+        return randomTitle;
+        console.log(randomTitle);
     }
+        console.log(randomTitleGetter());
+        document.getElementsByClassName("jumbotronText")[0].innerHTML = randomTitleGetter();
+      }
   });
-});
+};
