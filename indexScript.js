b0bb310abdb265294185fdf93254b00c7a0e5536
ismael perez <ismaelperezemail@gmail.com>
2017-07-18 20:08:35 -0700

    added functionality to link in jumbotron

--- indexScript.js
+++ indexScript.js
@@ -3,28 +3,38 @@ window.onload = function() {
     url: "https://content.guardianapis.com/search?q=technology&api-key=3cacc75f-61ed-4cf7-b623-78c2af06d49d",
     success: function(data) {
 
-      var articleArr = [];
-      var storyTitleArray = [];
 
-      function randomTitleGetter() {
+      var articleObjArr = [];
+      var storyTitleUrlArray = [];
+
+      function randomTitleUrlGetter() {
         for (var i = 0; i < data.response.results.length; i++) {
-          var retrievedData = data.response.results;
-          var retrievedDataTitle = data.response.results[0].webTitle;
+          var articleObj = {};
+          var retrievedData = data.response.results[i];
+          var retrievedDataTitle = retrievedData.webTitle;
+          var retrievedDataUrl = retrievedData.webUrl;
 
-          articleArr.push(retrievedData[i]);
-          storyTitleArray.push(articleArr[i].webTitle);
+          articleObj.title = retrievedDataTitle;
+          articleObj.url = retrievedDataUrl;
+          //console.log(articleObj);
+          articleObjArr.push(articleObj);
+          //console.log(articleObjArr);
+          storyTitleUrlArray.push(articleObjArr[i]);
+
         }
-        var randomTitle = storyTitleArray[Math.floor(Math.random() * storyTitleArray.length)];
+        var randomTitle = storyTitleUrlArray[Math.floor(Math.random() * storyTitleUrlArray.length)];
+        console.log(randomTitle.title);
+        console.log(randomTitle.url);
         return randomTitle;
       }
-        console.log(randomTitleGetter());
+        randomTitleUrlGetter();
 
         document.getElementsByClassName("generateButton")[0].addEventListener("click", function() {
-        document.getElementsByClassName("jumbotronText")[0].innerHTML = randomTitleGetter();
+        var randomizedObject = randomTitleUrlGetter();
+        document.getElementsByClassName("jumbotronText")[0].innerHTML = randomizedObject.title;
         document.getElementsByClassName("generateButton")[0].innerHTML = "Next Story";
+        document.getElementsByClassName("linkText")[0].href = randomizedObject.url;
       });
-
-
     }
   });
 };
